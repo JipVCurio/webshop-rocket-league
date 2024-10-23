9423e3dc2506fd15c3c181f5f4b59fa16b9a1aa0
JipVCurio <jv49@curio.nl>
2024-10-23 15:34:59 +0200

    improved the homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="fullscreen-cover">
+    <div class="container video-message-container">
+        <h2>Now available</h2>
+        <h3>Lamborghini Countach LPI 800-4</h3>
+        <a href="/products"><button class="btn">Order now</button></a>
+    </div>
+    
+</div>
 <div class="fullscreen-video-wrapper">
     <div class="fullscreen-video-container" style="background-image: url('{{ asset('/media/gifs/homepage-animation.gif') }}');">
-        <iframe src="https://www.youtube.com/embed/FODkiJMmals?si=rnVOLeN1KbOhh3h8&autoplay=1&mute=1&loop=1&playlist=FODkiJMmals"
+        <iframe src="https://www.youtube.com/embed/bn2cgd0A3no?si=qq2tySb_PHg1V8Aa&autoplay=1&mute=1&loop=1&playlist=bn2cgd0A3no"
             class="fullscreen-video"
             frameborder="0" 
             title="YouTube video player" 
