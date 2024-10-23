1f21208038b93b72f6c3451e1a8cd584e80a3768
JipVCurio <jv49@curio.nl>
2024-10-23 13:15:28 +0200

    producten display

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,7 +1,7 @@
-<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm nav-main">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm nav-main">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Rocket league webshop') }}
+            Home
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="fullscreen-animated-image" style="background-image: url('{{ asset('/media/gifs/homepage-animation.gif') }}');">
-    <div class="container fullscreen-image-content">
-        <h3>Nieuw</h3>
+<div class="fullscreen-video-wrapper">
+    <div class="fullscreen-video-container" style="background-image: url('{{ asset('/media/gifs/homepage-animation.gif') }}');">
+        <iframe src="https://www.youtube.com/embed/FODkiJMmals?si=rnVOLeN1KbOhh3h8&autoplay=1&mute=1&loop=1&playlist=FODkiJMmals"
+            class="fullscreen-video"
+            frameborder="0" 
+            title="YouTube video player" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" 
+        />
     </div>
-</div>
-<div class="container">
-    
 </div>
 @endsection
