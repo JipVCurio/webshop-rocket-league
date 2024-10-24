5258755f7d0755120b63e333ecfe794f8e691d90
JipVCurio <jv49@curio.nl>
2024-10-24 11:20:49 +0200

    details view implemented

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="container">
+    <div class="product-display-container">
+        <h2>{{$product->name}}</h2>
+        <div class="product-display-image-container">
+            <img src={{$product->imageUrl}}>
+        </div>
+        <div class="product-details-container">
+            <h3>{{$product->rarity}} {{$product->type}}</h3>
+            <p>{{$product->description}}</p>
+            <h4>€{{$product->price}}</h4>
+            <a><button class="btn">Add to cart</button></a>
+        </div>
+    </div>
+</div>
 @endsection
